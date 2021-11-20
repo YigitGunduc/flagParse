12210857aa4a09261e7202ac9afb7705c40ad041
Yigit Gunduc <ygunduc@gmail.com>
2021-11-20 18:12:47 +0300

    general update

--- flags.c
+++ flags.c
@@ -2,37 +2,45 @@
 #include <malloc.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h> 
 
-#define MAX_FLAG_LENGTH 256
+#define MAX_NUMBER_OF_FLAGS 32
 
 typedef enum {
-    FLAG_STR = 0,
-    FLAG_FLOAT,
-    FLAG_BOOL,
-    FLAG_UINT64,
-    FLAG_UINT32,
-    FLAG_DOUBLE,
+  FLAG_BOOL = 0,
+  FLAG_UINT,
+  FLAG_DOUBLE,
+  FLAG_STR,
 } Flag_Type;
 
 typedef struct {
-  char* desc;
-	Flag_Type type;
-	Flag_Type value;
+  bool bool_val;
+  double double_val;
+  char *str_val;
+  unsigned int uint_val;
+} flag_value;
+
+typedef struct {
+  char *desc;
   char *name;
+  Flag_Type type;
+  flag_value value;
 } flag_t;
+
+static flag_t flags[MAX_NUMBER_OF_FLAGS];
 
 void parser_args(int argc, char *argv[], char flag[])
 {
-    for (int i = 1; i < argc; i++) 
+  for (int i = 1; i < argc; i++) 
+  {
+    if (strcmp(argv[i], flag) == 0)
     {
-      if (strcmp(argv[i], flag) == 0)
-      {
-        puts("found");
-      }
+      puts("found");
     }
+  }
 }
 
-flag_t* add_flag(char *name, char *desc, Flag_Type type)
+flag_t *add_flag(char *name, char *desc, Flag_Type type)
 {
   flag_t *flag = malloc(sizeof(flag_t));
   flag->name = name;
@@ -44,8 +52,8 @@ flag_t* add_flag(char *name, char *desc, Flag_Type type)
 
 int main(int argc, char* argv[])
 {
-    flag_t* flag = add_flag("flag name", "flag desc", 1);
-    printf("%s\n", flag->name);
+  flag_t* flag = add_flag("--flagname", "flagdesc", 1);
+  printf("%s\n", flag->name);
 
-    return 0;
+  return 0;
 }
